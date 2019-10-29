708dae6a574f06845f4f5d078ad68f0eb6278711
joaoedc <39487274+joaoedc@users.noreply.github.com>
2019-10-29 19:31:41 -0300

    Add files via upload

--- Lista9/Arvore.java
+++ Lista9/Arvore.java
@@ -3,30 +3,55 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Lista9;
+package lista9;
 
 /**
  *
  * @author jecunha
  */
 public class Arvore<T> {
-    
-    private T raiz;
+
+    private NoArvore raiz;
 
     public Arvore() {
+        raiz = null;
     }
 
-    
-    
-    public T getRaiz() {
+    public NoArvore<T> getRaiz() {
         return raiz;
     }
 
-    public void setRaiz(T raiz) {
+    public void setRaiz(NoArvore raiz) {
         this.raiz = raiz;
     }
+
+    public String toString() {
+        if (raiz != null) {
+            return obterRepresentcaoTextual(raiz);
+        } else
+            return "";
+
+    }
+
+    private String obterRepresentcaoTextual(NoArvore<T> no){
+        NoArvore<T> p;
+        
+        String s = "<";
+        s += no.getInfo();
+        
+        p = no.getPrimeiro();
+        
+        while (p != null){
+            s += obterRepresentcaoTextual(p);
+            p = p.getProximo();
+        }
+        s += ">";
+        return s;
+    }
     
-    
+    public boolean pertence(NoArvore<T> p){
+        return buscar(raiz, info) != null;
+    }
     
     
     
--- Lista9/NoArvore.java
+++ Lista9/NoArvore.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Lista9;
+package lista9;
 
 /**
  *
@@ -14,16 +14,41 @@ public class NoArvore<T> {
     private T info;
     private NoArvore primeiro;
     private NoArvore proximo;
+    
 
     public NoArvore(T info) {
         this.info = info;
         this.primeiro = null;
         this.proximo = null;
     }
+    
+    public void inserirFilho(NoArvore<T> sa){
+        sa.setProximo(primeiro);
+        setPrimeiro(sa);
+    }
+    
+    public void setInfo(T info){
+        this.info = info;
+    }
+    
+    public T getInfo(){
+        return info;
+    }
 
-    public void inserirFilho(NoArvore sa){
-        sa.proximo = primeiro;
-        primeiro = sa;
+    public NoArvore getPrimeiro() {
+        return primeiro;
+    }
+
+    public void setPrimeiro(NoArvore primeiro) {
+        this.primeiro = primeiro;
+    }
+
+    public NoArvore getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(NoArvore proximo) {
+        this.proximo = proximo;
     }
     
     
